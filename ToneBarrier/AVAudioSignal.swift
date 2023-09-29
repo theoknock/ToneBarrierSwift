a8e8d794a4041c216c4fba2ca21fbccbdc5a72ec
James Alan Bush <jamesalanbush1972@gmail.com>
2023-09-29 09:33:29 -0400

    !!!

--- ToneBarrier/AVAudioSignal.swift
+++ ToneBarrier/AVAudioSignal.swift
@@ -14,26 +14,9 @@ import ObjectiveC
 import Dispatch
 import Accelerate
 
-var frequency = 220.0
+var frequency = 440.0
 var harmonic  = frequency * (5.0/4.0)
-
-var frame: AVAudioFramePosition = 0
-var frame_t: UnsafeMutablePointer<AVAudioFramePosition> = UnsafeMutablePointer(&frame)
-var n_time: simd_double1 = .zero
-var n_time_t: UnsafeMutablePointer<simd_double1> = UnsafeMutablePointer(&n_time)
-
-//var normalized_times_ref: UnsafeMutablePointer<simd_double1>? = nil;
-//var normalized_times: (AVAudioFrameCount) -> UnsafeMutablePointer<simd_double1>? = { (frame_count) in
-//    var normalized_time = [simd_double1](repeating: .zero, count: Int(frame_count))
-//    normalized_times_ref = UnsafeMutablePointer(mutating: normalized_time)
-//    
-//    for frame in stride(from: frame_t.pointee, to: frame_count, by: 1) {
-//        n_time_t.pointee = 0.0 + (((frame - 0.0) * (1.0 - 0.0))) / (Double(~frame_count) - 0.0)
-//        normalized_times_ref?.advanced(by: Int(frame)).pointee = n_time_t.pointee
-//    }
-//    
-//    return normalized_times_ref
-//}
+let tau = 2.0 * .pi
 
 @objc class AVAudioSignal: NSObject {
     private static let shared = AVAudioSignal()
@@ -44,35 +27,52 @@ var n_time_t: UnsafeMutablePointer<simd_double1> = UnsafeMutablePointer(&n_time)
     
     override init() {
         let main_mixer_node = audio_engine.mainMixerNode
-        let output_node = audio_engine.outputNode
-        let audio_format = output_node.outputFormat(forBus: 0)
-//        normalized_times(AVAudioFrameCount(audio_format.sampleRate))  
+        let output_node     = audio_engine.outputNode
+        let audio_format    = output_node.outputFormat(forBus: 0)
+        let frame_count     = Float(audio_format.sampleRate) * Float(audio_format.channelCount)
+        
+        func createSignal(frameCount: Int, frequency: Float) -> [Float] {
+            let inputSignal = (0 ..< frameCount).map {
+//                let x = 0.0 + (((Float($0) - 0.0) * (1.0 - 0.0))) / (Float(~frameCount) - 0.0)
+                var x = Float($0) / Float(frameCount)
+//                var x: Float = 0.0
+//                x = 0.0 + (((x - 0.0) * (1.0 - 0.0))) / (Float(~frameCount) - 0.0)
+//                debugPrint("x == \(Float($0))")
+                // print x t see if it is counting from 0 to the number of frames or...
+                // ...
+                return 2.0 * sin(Float(tau) * x * frequency) // sin((Float(frequency) / Float(frameCount)) * x)
+            }
+            return inputSignal
+        }
+        
+        func multiplySignals(array1: [Float], array2: [Float]) -> [Float] {
+            let result = zip(array1, array2).map(*)
+            return result
+        }
+        
+        func performCalculation(frameCount: Int, array1: [Float], array2: [Float]) -> [Float] {
+            guard array1.count == array2.count else {
+                return []
+            }
+            var result: [Float] = []
+            for i in 0..<frameCount {
+                result.append(array1[i] * array2[i]) // Change this line to perform your desired operation
+            }
+            return result
+        }
+
         
         self.audio_source_node_renderer = { _, _, frameCount, audioBufferList in
-//            let normalizedTime = (0 ..< Int(frameCount)).map {
-//                let x = 0.0 + (((Float($0) - 0.0) * (1.0 - 0.0))) / (Double(~frameCount) - 0.0) // Float($0)
-//                return sin((Float(frequency) / Float(frameCount)) * x)
-//            }
-            
             // To-do: Replace with: amplitude • cos((tau •  x) / period)
-            let inputSignal = (0 ..< Int(frameCount)).map {
-                let x = Float($0)
-                return sin((Float(frequency) / Float(frameCount)) * x)
-            }
-            
-            /*
-                             ((2.0 * M_PI) / frameCount) * frequency;
-                             ((2.0 * M_PI) / frameCount) * harmonic;
-                             let a = sin((Float(frequency) / Float(frameCount)) * x)
-                             let b = sin((Float(harmonic)  / Float(frameCount)) * x)
-                             return (2.0 * (sin(a + b) * cos(a - b))) / 2.0
-             */
+            let frequencySignal = createSignal(frameCount: Int(frameCount), frequency: Float(frequency))
+            let amplitudeSignal = createSignal(frameCount: Int(frameCount), frequency: Float(harmonic))
+            let signalSamples   = performCalculation(frameCount: Int(frameCount), array1: frequencySignal, array2: amplitudeSignal)
             
             let ablPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
             for buffer in ablPointer {
                 let buf: UnsafeMutableBufferPointer<Float> = UnsafeMutableBufferPointer(buffer)
-                inputSignal.withUnsafeBufferPointer { sourceBuffer in
-                    buf.baseAddress!.initialize(from: sourceBuffer.baseAddress!, count: inputSignal.count)
+                signalSamples.withUnsafeBufferPointer { sourceBuffer in
+                    buf.baseAddress!.initialize(from: sourceBuffer.baseAddress!, count: Int(frameCount))
                 }
             }
             return noErr
--- ToneBarrier/CodeSnippetsStandby.md
+++ ToneBarrier/CodeSnippetsStandby.md
@@ -1,2 +1,38 @@
-#  <#Title#>
+# Normalized time (array of pointers)    
+    
+    var frame: AVAudioFramePosition = 0
+    var frame_t: UnsafeMutablePointer<AVAudioFramePosition> = UnsafeMutablePointer(&frame)
+    var n_time: Float = .zero
+    var n_time_t: UnsafeMutablePointer<Float> = UnsafeMutablePointer(&n_time)
+    
+    var normalized_times_ref: UnsafeMutablePointer<Float>? = nil;
+    var normalized_times: (AVAudioFrameCount) -> UnsafeMutablePointer<Float>? = { (frame_count) in
+        var normalized_time = [Float](repeating: .zero, count: Int(frame_count))
+        normalized_times_ref = UnsafeMutablePointer(mutating: normalized_time)
+        
+        for frame in stride(from: frame_t.pointee, to: frame_count, by: 1) {
+            n_time_t.pointee = 0.0 + (((frame - 0.0) * (1.0 - 0.0))) / (Float(~frame_count) - 0.0)
+            normalized_times_ref?.advanced(by: Int(frame)).pointee = n_time_t.pointee
+        }
+        
+        return normalized_times_ref
+    }
+    
+    
+    # Normalized time (Mapped [Float])
+    
+    normalized_times(AVAudioFrameCount(audio_format.sampleRate))
+    
+    let normalizedTime = (0 ..< Int(frameCount)).map {
+        let x = 0.0 + (((Float($0) - 0.0) * (1.0 - 0.0))) / (Double(~frameCount) - 0.0) // Float($0)
+        return sin((Float(frequency) / Float(frameCount)) * x)
+    }
 
+# Channel mixer
+
+            ((2.0 * M_PI) / frameCount) * frequency;
+            ((2.0 * M_PI) / frameCount) * harmonic;
+            let a = sin((Float(frequency) / Float(frameCount)) * x)
+            let b = sin((Float(harmonic)  / Float(frameCount)) * x)
+            return (2.0 * (sin(a + b) * cos(a - b))) / 2.0
+            
