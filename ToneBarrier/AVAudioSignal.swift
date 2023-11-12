540d2821141a85bb6ebc08977da11ecfd5ccf79c
James Alan Bush <jamesalanbush1972@gmail.com>
2023-11-12 16:34:13 -0500

    Generates random frequencies, swaps them out after two seconds

--- ToneBarrier/AVAudioSignal.swift
+++ ToneBarrier/AVAudioSignal.swift
@@ -15,7 +15,7 @@ import Dispatch
 import Accelerate
 
 var root: Float32     = Float32(440.0)
-let harmonic: Float32 = Float32(440.0 * (5.0/4.0))
+var harmonic: Float32 = Float32(440.0 * (5.0/4.0))
 let amplitude: Float32 = Float32(0.5)
 let tau: Float32      = Float32(2.0 * Float32.pi)
 var frame: AVAudioFramePosition = Int64.zero
@@ -34,31 +34,54 @@ var normalized_times_ref: UnsafeMutablePointer<Float32>? = nil;
     override init() {
         let main_mixer_node: AVAudioMixerNode = audio_engine.mainMixerNode
         let audio_format: AVAudioFormat       = AVAudioFormat(standardFormatWithSampleRate: audio_engine.mainMixerNode.outputFormat(forBus: 0).sampleRate, channels: audio_engine.mainMixerNode.outputFormat(forBus: 0).channelCount )!
-        let buffer_length: Int = Int(audio_format.sampleRate) // * Int(audio_format.channelCount) * Int(2)
-
+        let buffer_length: Int = Int(audio_format.sampleRate) * Int(audio_format.channelCount)
+        
         
         func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float32, max_old: Float32) -> Float32 {
             let val_new = min_new + ((((val_old - min_old) * (max_new - min_new))) / (max_old - min_old));
             return val_new;
         }
         
-        func generateFrequencies(root_frequency: Float32, harmonic_factor: Float32, frame_count: Int) -> [Float32] {
-            var root_frequency_samples: [Float32]  = [Float32](repeating: Float32.zero, count: frame_count)
-            var harmonic_factor_samples: [Float32] = [Float32](repeating: Float32.zero, count: frame_count)
-            let combinedSamples                  = (Int.zero ..< frame_count).map { i in
-                let time: Float32                  = Float32(scale(min_new: -1.0, max_new: 1.0, val_old: Float32(i), min_old: -1.0, max_old: Float32(frame_count - 1))) //Float32(~(-frame_count))))
-                root_frequency_samples[i]        = cosf(tau * time * root_frequency)
-                harmonic_factor_samples[i]       = cosf(tau * time * harmonic_factor)
-                return root_frequency_samples[i] + harmonic_factor_samples[i];
-            }
-            
-            return combinedSamples
+/** --------------------------------  **/
+        
+        enum MusicalNoteFrequency: UInt {
+            case a = 440
+            case bFlat = 466
+            case b = 494
+            case c = 523
+            case cSharp = 554
+            case d = 587
+            case dSharp = 622
+            case e = 659
+            case f = 698
+            case fSharp = 740
+            case g = 784
+            case aFlat = 831
         }
+        
+        let noteFrequency: (MusicalNoteFrequency) -> Double = { musicalNote in
+            let randomNote = pow(2.0, Double(musicalNote.rawValue) / 12.0) * 440.0
+            return randomNote
+        }
+        
+        enum CombinationTone: UInt {
+            case CombinationToneRoot
+            case CombinationToneFifth
+            case CombinationToneOctave
+            case CombinationToneRandom
+        };
+        
+        func randomPianoNoteFrequency() -> Float32 {
+            let n = Int.random(in: 1...88) - 49
+            return 440 * pow(2, Float32(n) / 12.0)
+        }
+        
+/** --------------------------------  **/
         
         func makeIncrementerWithReset(maximumValue: Int) -> ((Int) -> [Int]) {
             var counter = 0
             let counter_max = maximumValue
-
+            
             func incrementCounter(count: Int) -> [Int] {
                 var numbersArray = [Int](repeating: 0, count: count)
                 for index in (0 ..< count) {
@@ -68,49 +91,103 @@ var normalized_times_ref: UnsafeMutablePointer<Float32>? = nil;
                     if counter == counter_max {
                         counter = 0
                     }
-//                    print("\(index)\t\(value)")
+                    //                    print("\(index)\t\(value)")
                 }
                 return numbersArray
             }
-
+            
             return incrementCounter
         }
-
-        let incrementer = makeIncrementerWithReset(maximumValue: buffer_length)
-
-        var currentPhase: Float32   = Float32.zero
-        var phaseIncrement: Float32 = (tau / Float32(audio_format.sampleRate)) * root
-        var currentPhase_h: Float32   = Float32.zero
-        var phaseIncrement_h: Float32 = phaseIncrement //(tau / Float32(audio_format.sampleRate)) * root + (Float32.pi / Float32(2.0))
         
-        func generateFrequency(frame_count: Int) -> [Float32] {
-            var frequency_samples: [Float32] = [Float32](repeating: Float32.zero, count: frame_count)
-            var frequency_samples_h: [Float32] = [Float32](repeating: Float32.zero, count: frame_count)
-            let frame_indicies = incrementer(frame_count)
-            let signal_samples = (Int.zero ..< frame_count).map { i in
-                // get value at i in frame_indicies and set phaseIncrement to a new value if frame_indicies[i] == 0
-                if frame_indicies[i] == 0 {
-//                    print(frame_indicies[i])
-                    root = Float32(root * (5.0/4.0))
-                    phaseIncrement = (tau / Float32(audio_format.sampleRate)) * root
-                    phaseIncrement_h = phaseIncrement
+        let incrementer = makeIncrementerWithReset(maximumValue: buffer_length)
+        var currentPhase:     [Float32] = [Float32.zero, Float32.zero]
+        var phaseIncrement:   [Float32] = [tau / Float32(buffer_length), tau / Float32(buffer_length)]
+        var signalPhase:      [Float32] = [Float32.zero, Float32.zero]
+        var signalIncrement:  [Float32] = [Float32.zero, Float32.zero]
+        var signalFrequency:  [Float32] = [Float32.zero, Float32.zero]
+        
+        func sample_buffers(frame_count: Int) -> [([Float32])] {
+            var buffers: [([Float32])] = [[Float32](repeating: Float32.zero, count: frame_count), [Float32](repeating: Float32.zero, count: frame_count)]
+            return buffers.map { innerArray in
+                (Int.zero ..< frame_count).map {
+                    return Float32($0)
                 }
-                frequency_samples[i] = sin(currentPhase) * amplitude
-                frequency_samples_h[i] = sin(currentPhase_h) * amplitude
-                
-                defer {
-                    (currentPhase = ((currentPhase + phaseIncrement)).truncatingRemainder(dividingBy: tau))
-                    (currentPhase_h = ((currentPhase_h + phaseIncrement_h)).truncatingRemainder(dividingBy: tau))
-                 }
-                
-                return frequency_samples[i] + frequency_samples_h[i];
             }
-            return signal_samples
         }
+//            return (Int.zero ..< frame_count).map { i in
+//                if i == 0 {
+//                    signalFrequency = [randomPianoNoteFrequency(), randomPianoNoteFrequency()]
+//                    signalIncrement = [signalFrequency[0] * phaseIncrement[0], signalFrequency[1] * phaseIncrement[1]]
+//                }
+//                
+//                return (Int.zero ..< 2).map { j in
+//                    //                    signalPhase = [sin(signalPhase[0]), sin(signalPhase[1])]
+//                    //                    frequency_samples = [[signalPhase[0]], [signalPhase[1]]] //sin(signalPhase[0])[0], sin(signalPhase[1])[1]]
+//                    //                    signalPhase       = [(signalPhase[0] + signalIncrement[0]), (signalPhase[0] + signalIncrement[0])]
+//                    //
+//                    //                    defer {
+//                    //                        //                    if currentPhase >= tau { currentPhase -= tau }
+//                    //                        //                    if currentPhase < Float32.zero { currentPhase += tau }
+//                    //                    }
+//                    
+//                    return Float32.zero
+//                }
+//                return [Float32]
+//            }
+//        }
+//        
+//        func generateFrequency(frame_count: Int) -> [Float32] {
+//            //            var frequency_samples = Array(repeating: Array(repeating: Float32.zero, count: 2), count: 2)
+//            var frequency_samples: [([Float32])] = [[Float32](repeating: Float32.zero, count: frame_count), [Float32](repeating: Float32.zero, count: frame_count)]
+//            let frame_indicies = incrementer(frame_count)
+//            let signal_samples: [([Float32])] = (Int.zero ..< frame_count).map { i in
+//                // get value at i in frame_indicies and set phaseIncrement to a new value if frame_indicies[i] == 0
+//                if i == 0 {
+//                    signalFrequency = [randomPianoNoteFrequency(), randomPianoNoteFrequency()]
+//                    signalIncrement = [signalFrequency[0] * phaseIncrement[0], signalFrequency[1] * phaseIncrement[1]]
+//                }
+//                
+//                signalPhase = [sin(signalPhase[0]), sin(signalPhase[1])]
+//                frequency_samples = [[signalPhase[0]], [signalPhase[1]]] //sin(signalPhase[0])[0], sin(signalPhase[1])[1]]
+//                signalPhase       = [(signalPhase[0] + signalIncrement[0]), (signalPhase[0] + signalIncrement[0])]
+//                
+//                defer {
+//                    //                    if currentPhase >= tau { currentPhase -= tau }
+//                    //                    if currentPhase < Float32.zero { currentPhase += tau }
+//                }
+//                
+//                return frequency_samples
+//            }
+//            
+//            return signal_samples
+//        }
+        
+/** --------------------------------  **/
                 
-        let audio_source_node: AVAudioSourceNode = AVAudioSourceNode(format: audio_format, renderBlock: { _, _, frameCount, audioBufferList in
+                func generateFrequencies(frame_count: Int) -> [Float32] {
+                    let frame_indicies                     = incrementer(frame_count)
+                    var root_frequency_samples: [Float32]  = [Float32](repeating: Float32.zero, count: frame_count)
+                    var harmonic_factor_samples: [Float32] = [Float32](repeating: Float32.zero, count: frame_count)
+                    let combinedSamples                    = (Int.zero ..< frame_count).map { i in
+                        if frame_indicies[i] == 0 {
+                            root = randomPianoNoteFrequency()
+                            harmonic = randomPianoNoteFrequency()
+                        }
+                        let time: Float32                = Float32(scale(min_new: -1.0, max_new: 1.0, val_old: Float32(i), min_old: -1.0, max_old: Float32(frame_count - 1))) //Float32(~(-frame_count))))
+                        root_frequency_samples[i]        = cosf(tau * time * root)
+                        harmonic_factor_samples[i]       = cosf(tau * time * harmonic)
+                        
+                        return root_frequency_samples[i] + harmonic_factor_samples[i];
+                    }
+                    
+                    return combinedSamples
+                }
 
-            let signalSamples    = generateFrequency(frame_count: Int(frameCount)) //generateFrequencies(root_frequency: root, harmonic_factor: harmonic, frame_count: Int(frameCount))
+/** --------------------------------  **/
+        
+        let audio_source_node: AVAudioSourceNode = AVAudioSourceNode(format: audio_format, renderBlock: { _, _, frameCount, audioBufferList in
+            
+            let signalSamples    = generateFrequencies(frame_count: Int(frameCount)) //generateFrequencies(root_frequency: root, harmonic_factor: harmonic, frame_count: Int(frameCount))
             let ablPointer       = UnsafeMutableAudioBufferListPointer(audioBufferList)
             let leftChannelData  = ablPointer[0]
             let rightChannelData = ablPointer[1]
