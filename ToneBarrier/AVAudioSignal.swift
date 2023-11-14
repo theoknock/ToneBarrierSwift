57470a2be7b11f5255f8f0dd47647934296695bd
James Alan Bush <jamesalanbush1972@gmail.com>
2023-11-13 20:16:43 -0500

    Add note random-generator function

--- ToneBarrier/AVAudioSignal.swift
+++ ToneBarrier/AVAudioSignal.swift
@@ -13,6 +13,7 @@ import Combine
 import ObjectiveC
 import Dispatch
 import Accelerate
+import GameKit
 
 var root:         Float32 = Float32(440.0)
 var harmonic:     Float32 = Float32(440.0 * (5.0/4.0))
@@ -26,6 +27,12 @@ var n_time_t: UnsafeMutablePointer<Float32> = UnsafeMutablePointer(&n_time)
 var normalized_times_ref: UnsafeMutablePointer<Float32>? = nil;
 
 
+func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float32, max_old: Float32) -> Float32 {
+    let val_new = min_new + ((((val_old - min_old) * (max_new - min_new))) / (max_old - min_old));
+    return val_new;
+}
+
+
 @objc class AVAudioSignal: NSObject {
     private static let shared = AVAudioSignal()
     
@@ -35,44 +42,80 @@ var normalized_times_ref: UnsafeMutablePointer<Float32>? = nil;
     override init() {
         let main_mixer_node: AVAudioMixerNode = audio_engine.mainMixerNode
         let audio_format: AVAudioFormat       = AVAudioFormat(standardFormatWithSampleRate: audio_engine.mainMixerNode.outputFormat(forBus: 0).sampleRate, channels: audio_engine.mainMixerNode.outputFormat(forBus: 0).channelCount )!
-        let buffer_length: Int = Int(audio_format.sampleRate) * Int(audio_format.channelCount)
+        let buffer_length: Int = Int(audio_format.sampleRate)
+        * Int(audio_format.channelCount)
         
-        let gaussianDistribution: (Float32, Float32, Float32) -> Float32 = { x, mean, variance in
-            let numerator = pow(x - mean, 2)
-            let denominator = 2 * pow(variance, 2)
-            let exponent = -numerator / denominator
-            let gaussianValue = exp(exponent)
-
-            return gaussianValue
+        
+        /** --------------------------------  **/
+        
+        enum CombinationTone {
+            enum CombinationToneArpeggio: UInt {
+                case CombinationToneNone
+                case CombinationToneArpeggioRootOrDrone
+                case CombinationToneArpeggioFifth
+                case CombinationToneArpeggioOctave
+                case CombinationToneArpeggioRandom
+            }
+            
+            enum CombinationToneSum: UInt {
+                case CombinationToneSumNone
+                case CombinationToneRoot
+                case CombinationToneArpeggioFifth
+                case CombinationToneArpeggioOctave
+                case CombinationToneSumRandom
+            }
+            
+            enum CombinationToneUnitFrequency: UInt {
+                case CombinationToneNone
+                case CombinationTonePianoNote
+                case CombinationToneHertz
+            }
+            
+            enum MusicalNote: UInt {
+                case MusicalNoteA
+                case MusicalNoteBFlat
+                case MusicalNoteB
+                case MusicalNoteC
+                case MusicalNoteCSharp
+                case MusicalNoteD
+                case MusicalNoteDSharp
+                case MusicalNoteE
+                case MusicalNoteF
+                case MusicalNoteFSharp
+                case MusicalNoteG
+                case MusicalNoteAFlat
+            };
         }
-
         
-        func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float32, max_old: Float32) -> Float32 {
-            let val_new = min_new + ((((val_old - min_old) * (max_new - min_new))) / (max_old - min_old));
-            return val_new;
+        
+        var generateNormalizedRandom: () -> (() -> Float32) = {
+            var generator: RandomNumberGenerator = SystemRandomNumberGenerator()
+            var random: Float32 = Float32.zero // Use a exponential or logarithmic curve to weight results
+            return {
+                return {
+                    random = Float32.random(in: (0.0...1.0), using: &generator)
+                    return random
+                }
+            }()
         }
         
-/** --------------------------------  **/
-        
-        enum CombinationTone: UInt {
-            case CombinationToneRoot
-            case CombinationToneFifth
-            case CombinationToneOctave
-            case CombinationToneRandom
-        };
-        
+        let randomizer = generateNormalizedRandom()
 
-        
         func randomPianoNoteFrequency() -> Float32 {
-            let r: Float32 = pow(Float32.random(in: 0.15...1.0), 0.875) * 50
-            return 440 * pow(2, floor(r) / 12.0)
+            let note: Float32 = floor(randomizer() * 88.0)
+            let frequency = 440.0 * pow(2.0, (note - 49.0) / 12.0)
+            print("\(note)\t\(frequency)\n")
+            
+            return frequency
         }
         
-/** --------------------------------  **/
+        /** --------------------------------  **/
         
         func makeIncrementerWithReset(maximumValue: Int) -> ((Int) -> [Int]) {
             var counter = 0
             let counter_max = maximumValue
+            
+            // Initialize "normalized time" array here
             
             func incrementCounter(count: Int) -> [Int] {
                 var numbersArray = [Int](repeating: 0, count: count)
@@ -112,79 +155,31 @@ var normalized_times_ref: UnsafeMutablePointer<Float32>? = nil;
                 }
             }
         }
-//            return (Int.zero ..< frame_count).map { i in
-//                if i == 0 {
-//                    signalFrequency = [randomPianoNoteFrequency(), randomPianoNoteFrequency()]
-//                    signalIncrement = [signalFrequency[0] * phaseIncrement[0], signalFrequency[1] * phaseIncrement[1]]
-//                }
-//                
-//                return (Int.zero ..< 2).map { j in
-//                    //                    signalPhase = [sin(signalPhase[0]), sin(signalPhase[1])]
-//                    //                    frequency_samples = [[signalPhase[0]], [signalPhase[1]]] //sin(signalPhase[0])[0], sin(signalPhase[1])[1]]
-//                    //                    signalPhase       = [(signalPhase[0] + signalIncrement[0]), (signalPhase[0] + signalIncrement[0])]
-//                    //
-//                    //                    defer {
-//                    //                        //                    if currentPhase >= tau { currentPhase -= tau }
-//                    //                        //                    if currentPhase < Float32.zero { currentPhase += tau }
-//                    //                    }
-//                    
-//                    return Float32.zero
-//                }
-//                return [Float32]
-//            }
-//        }
-//        
-//        func generateFrequency(frame_count: Int) -> [Float32] {
-//            //            var frequency_samples = Array(repeating: Array(repeating: Float32.zero, count: 2), count: 2)
-//            var frequency_samples: [([Float32])] = [[Float32](repeating: Float32.zero, count: frame_count), [Float32](repeating: Float32.zero, count: frame_count)]
-//            let frame_indicies = incrementer(frame_count)
-//            let signal_samples: [([Float32])] = (Int.zero ..< frame_count).map { i in
-//                // get value at i in frame_indicies and set phaseIncrement to a new value if frame_indicies[i] == 0
-//                if i == 0 {
-//                    signalFrequency = [randomPianoNoteFrequency(), randomPianoNoteFrequency()]
-//                    signalIncrement = [signalFrequency[0] * phaseIncrement[0], signalFrequency[1] * phaseIncrement[1]]
-//                }
-//                
-//                signalPhase = [sin(signalPhase[0]), sin(signalPhase[1])]
-//                frequency_samples = [[signalPhase[0]], [signalPhase[1]]] //sin(signalPhase[0])[0], sin(signalPhase[1])[1]]
-//                signalPhase       = [(signalPhase[0] + signalIncrement[0]), (signalPhase[0] + signalIncrement[0])]
-//                
-//                defer {
-//                    //                    if currentPhase >= tau { currentPhase -= tau }
-//                    //                    if currentPhase < Float32.zero { currentPhase += tau }
-//                }
-//                
-//                return frequency_samples
-//            }
-//            
-//            return signal_samples
-//        }
+        /** --------------------------------  **/
         
-/** --------------------------------  **/
-                
-                func generateFrequencies(frame_count: Int) -> [Float32] {
-                    let frame_indicies                     = incrementer(frame_count)
-                    var root_frequency_samples: [Float32]  = [Float32](repeating: Float32.zero, count: frame_count)
-                    var harmonic_factor_samples: [Float32] = [Float32](repeating: Float32.zero, count: frame_count)
-                    let combinedSamples                    = (Int.zero ..< frame_count).map { i in
-                        if frame_indicies[i] == 0 {
-                            print("\(frame_indicies[i])     \(i))")
-                            root = randomPianoNoteFrequency()
-                            harmonic = randomPianoNoteFrequency()
-                        }
-                        
-                        let time: Float32          = Float32(scale(min_new: 0.0, max_new: 1.0, val_old: Float32(frame_indicies[i]), min_old: 0.0, max_old: Float32(buffer_length))) //Float32(~(-frame_count))))
-                        root_frequency_samples[i]  = cos(tau * time * root)
-                        harmonic_factor_samples[i] = cos(tau * time * root + phase_offset)
-                        let scaledSamples          = Float32(scale(min_new: 0.0, max_new: 1.0, val_old: Float32(root_frequency_samples[i] + harmonic_factor_samples[i]), min_old: 0.0, max_old: 2.0)) //Float32(~(-frame_count))))
-                        
-                        return scaledSamples
-                    }
-                    
-                    return combinedSamples
+        func generateFrequencies(frame_count: Int) -> [Float32] {
+            let frame_indicies                     = incrementer(frame_count)
+            var root_frequency_samples: [Float32]  = [Float32](repeating: Float32.zero, count: frame_count)
+            var harmonic_factor_samples: [Float32] = [Float32](repeating: Float32.zero, count: frame_count)
+            let combinedSamples                    = (Int.zero ..< frame_count).map { i in
+                if frame_indicies[i] == 0 {
+                    print("\(frame_indicies[i])     \(i))")
+                    root = randomPianoNoteFrequency()
+                    harmonic = root * (3.0 / 2.0)
                 }
-
-/** --------------------------------  **/
+                
+                let time: Float32          = Float32(scale(min_new: 0.0, max_new: 1.0, val_old: Float32(frame_indicies[i]), min_old: 0.0, max_old: Float32(buffer_length))) //Float32(~(-frame_count))))
+                root_frequency_samples[i]  = cos(tau * time * root)
+                harmonic_factor_samples[i] = cos(tau * time * root + phase_offset)
+                let scaledSamples          = Float32(scale(min_new: 0.0, max_new: 1.0, val_old: Float32(root_frequency_samples[i] + harmonic_factor_samples[i]), min_old: 0.0, max_old: 2.0)) //Float32(~(-frame_count))))
+                
+                return scaledSamples
+            }
+            
+            return combinedSamples
+        }
+        
+        /** --------------------------------  **/
         
         let audio_source_node: AVAudioSourceNode = AVAudioSourceNode(format: audio_format, renderBlock: { _, _, frameCount, audioBufferList in
             
--- ToneBarrier/CodeSnippetsStandby.md
+++ ToneBarrier/CodeSnippetsStandby.md
@@ -102,3 +102,234 @@ func generateSignalSamples(frequency: Float, sampleRate: Float, duration: Float)
             print("change.newKey: \(String(describing: change?[.newKey]))")
         }
     }
+
+# Crappy, non-working randomizes and distributors
+
+        let distribution = GKGaussianDistribution(lowestValue: 1, highestValue: 8)
+                func redistributeValueUsingGaussianCurve(value: Float32, mean: Float32, standardDeviation: Float32) -> Float32 {
+                    // Calculate the z-score of the value.
+                    let zScore = (value - mean) / standardDeviation
+        
+                    // Calculate the probability of the value under the Gaussian curve.
+                    let probability = 1 / sqrt(2 * Float32.pi * standardDeviation * standardDeviation) * exp(-zScore * zScore / 2)
+        
+                    // Return the redistributed value.
+                    return probability * standardDeviation + mean
+                }
+        
+        
+        let gaussianDistribution: (Float32, Float32, Float32) -> Float32 = { t, m, v in
+            let standardDeviation = Float32(sqrt(v))
+            let normalizationFactor = 1 / (standardDeviation * sqrt(2 * Float32.pi))
+            let exponentNumerator = -pow(t - m, 2)
+            let exponentDenominator = 2 * v
+            let exponent = exponentNumerator / exponentDenominator
+            let gaussianValue = normalizationFactor * Float32(exp(exponent))
+            
+            return gaussianValue
+        }
+        
+        let gaussianDistribution_: (Float32, Float32, Float32) -> Float32 = { x, mean, variance in
+            let numerator = pow(x - mean, 2)
+            let denominator = 2 * pow(variance, 2)
+            let exponent = -numerator / denominator
+            let gaussianValue = exp(exponent)
+            
+            return gaussianValue
+        }
+        
+        
+        func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float32, max_old: Float32) -> Float32 {
+            let val_new = min_new + ((((val_old - min_old) * (max_new - min_new))) / (max_old - min_old));
+            return val_new;
+        }
+        
+        var generateNormalizedRandom: () -> (() -> Float) = {
+            srand48(Int(time(nil)))
+            var random: Float = 0.0
+            return {
+                return {
+                    random = Float(drand48())
+                    return random
+                }
+            }()
+        }
+        
+        
+                static Float32 (^(^generate_normalized_random)(void))(void) = ^{
+                    srand48((unsigned int)time(0));
+                    static Float32 random;
+                    return ^ (Float32 * random_t) {
+                        return ^ Float32 {
+                            return (*random_t = (drand48()));
+                        };
+                    }(&random);
+                };
+        
+                typedef typeof(Float32(^)(void)) random_generator;
+                typedef typeof(Float32(^(* restrict))(void)) random_n_t;
+                static Float32 (^(^(^(^generate_random)(Float32(^)(void)))(Float32(^)(Float32)))(Float32(^)(Float32)))(void) = ^ (Float32(^randomize)(void)) {
+                    return ^ (Float32(^distribute)(Float32)) {
+                        return ^ (Float32(^scale)(Float32)) {
+                                return ^ Float32 {
+                                    return scale(distribute(randomize()));
+                                };
+                        };
+                    };
+                };
+        
+        /** --------------------------------  **/
+        
+        enum CombinationTone {
+            enum CombinationToneArpeggio: UInt {
+                case CombinationToneNone
+                case CombinationToneArpeggioRootOrDrone
+                case CombinationToneArpeggioFifth
+                case CombinationToneArpeggioOctave
+                case CombinationToneArpeggioRandom
+            }
+            
+            enum CombinationToneSum: UInt {
+                case CombinationToneSumNone
+                case CombinationToneRoot
+                case CombinationToneArpeggioFifth
+                case CombinationToneArpeggioOctave
+                case CombinationToneSumRandom
+            }
+            
+            enum CombinationToneUnitFrequency: UInt {
+                case CombinationToneNone
+                case CombinationTonePianoNote
+                case CombinationToneHertz
+            }
+            
+            enum MusicalNote: UInt {
+                case MusicalNoteA
+                case MusicalNoteBFlat
+                case MusicalNoteB
+                case MusicalNoteC
+                case MusicalNoteCSharp
+                case MusicalNoteD
+                case MusicalNoteDSharp
+                case MusicalNoteE
+                case MusicalNoteF
+                case MusicalNoteFSharp
+                case MusicalNoteG
+                case MusicalNoteAFlat
+            };
+        }
+        
+        
+        
+        
+        func RandomNumberGenerator() -> () -> Float32 {
+            var descriptor = BNNSNDArrayDescriptor.allocateUninitialized(
+                scalarType: Float32.self,
+                shape: .vector(1, stride: Int.Stride()))
+            
+            let randomNumberGenerator = BNNSCreateRandomGenerator(
+                BNNSRandomGeneratorMethodAES_CTR,
+                nil)
+            
+            func generateRandomNumber() -> Float32 {
+                
+                BNNSRandomFillNormalFloat(randomNumberGenerator,
+                                          &descriptor,
+                                          0.5,
+                                          1.0)
+                
+                let bytesPointer = UnsafeMutableRawPointer.allocate(byteCount: Int(Float32.Stride()), alignment: 1)
+                bytesPointer.storeBytes(of: (descriptor.data?.load(fromByteOffset: 0, as: Float32.self))! , as: Float32.self)
+                let x = bytesPointer.load(as: Float32.self)
+                print("\(x)\n")
+                
+                return x
+            }
+            return generateRandomNumber
+        }
+        
+        let random_number = RandomNumberGenerator()
+        
+        
+        
+        func randomFloats(n: Int,
+                          mean: Float32,
+                          standardDeviation: Float32) -> [Float32] {
+            
+            let result = [Float](unsafeUninitializedCapacity: n) {
+                
+                buffer, unsafeUninitializedCapacity in
+                
+                guard
+                    var arrayDescriptor = BNNSNDArrayDescriptor(
+                        data: buffer,
+                        shape: .vector(n)),
+                    let randomNumberGenerator = BNNSCreateRandomGenerator(
+                        BNNSRandomGeneratorMethodAES_CTR,
+                        nil) else {
+                    fatalError()
+                }
+                
+                BNNSRandomFillNormalFloat(
+                    randomNumberGenerator,
+                    &arrayDescriptor,
+                    mean,
+                    standardDeviation)
+                
+                unsafeUninitializedCapacity = n
+                BNNSDestroyRandomGenerator(randomNumberGenerator)
+            }
+            return result
+        }
+
+
+# Signal-generator code
+
+        return (Int.zero ..< frame_count).map { i in
+            if i == 0 {
+                signalFrequency = [randomPianoNoteFrequency(), randomPianoNoteFrequency()]
+                signalIncrement = [signalFrequency[0] * phaseIncrement[0], signalFrequency[1] * phaseIncrement[1]]
+            }
+            
+            return (Int.zero ..< 2).map { j in
+                //                    signalPhase = [sin(signalPhase[0]), sin(signalPhase[1])]
+                //                    frequency_samples = [[signalPhase[0]], [signalPhase[1]]] //sin(signalPhase[0])[0], sin(signalPhase[1])[1]]
+                //                    signalPhase       = [(signalPhase[0] + signalIncrement[0]), (signalPhase[0] + signalIncrement[0])]
+                //
+                //                    defer {
+                //                        //                    if currentPhase >= tau { currentPhase -= tau }
+                //                        //                    if currentPhase < Float32.zero { currentPhase += tau }
+                //                    }
+                
+                return Float32.zero
+            }
+            return [Float32]
+        }
+    }
+    
+    func generateFrequency(frame_count: Int) -> [Float32] {
+        //            var frequency_samples = Array(repeating: Array(repeating: Float32.zero, count: 2), count: 2)
+        var frequency_samples: [([Float32])] = [[Float32](repeating: Float32.zero, count: frame_count), [Float32](repeating: Float32.zero, count: frame_count)]
+        let frame_indicies = incrementer(frame_count)
+        let signal_samples: [([Float32])] = (Int.zero ..< frame_count).map { i in
+            // get value at i in frame_indicies and set phaseIncrement to a new value if frame_indicies[i] == 0
+            if i == 0 {
+                signalFrequency = [randomPianoNoteFrequency(), randomPianoNoteFrequency()]
+                signalIncrement = [signalFrequency[0] * phaseIncrement[0], signalFrequency[1] * phaseIncrement[1]]
+            }
+            
+            signalPhase = [sin(signalPhase[0]), sin(signalPhase[1])]
+            frequency_samples = [[signalPhase[0]], [signalPhase[1]]] //sin(signalPhase[0])[0], sin(signalPhase[1])[1]]
+            signalPhase       = [(signalPhase[0] + signalIncrement[0]), (signalPhase[0] + signalIncrement[0])]
+            
+            defer {
+                //                    if currentPhase >= tau { currentPhase -= tau }
+                //                    if currentPhase < Float32.zero { currentPhase += tau }
+            }
+            
+            return frequency_samples
+        }
+        
+        return signal_samples
+    }
+    
