dc003ef1fb50d311592333fbf1d504948ca9ebce
James Alan Bush <jamesalanbush1972@gmail.com>
2023-11-15 09:44:44 -0500

    !!!!

--- ToneBarrier/AVAudioSignal.swift
+++ ToneBarrier/AVAudioSignal.swift
@@ -77,7 +77,7 @@ func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float3
         //        }
         
         
-        func scaled_random_generator_curve(mid: Float32, lower: Float32, upper: Float32) -> Float32 {
+        func scaled_random_generator_exp(mid: Float32, lower: Float32, upper: Float32) -> Float32 {
             var r: Float32 = Float32.random(in: (Float32.zero...1.0))
             var s: Float32 = scale(min_new: lower, max_new: upper, val_old: r, min_old: Float.zero, max_old: 1.0)
             
@@ -98,7 +98,7 @@ func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float3
         }
         
         func pianoNoteFrequency() -> Float32 {
-            let c: Float32 = scaled_random_generator_curve(mid: 0.666, lower: 0.333, upper: 0.888)
+            let c: Float32 = scaled_random_generator_exp(mid: 0.666, lower: 0.333, upper: 0.888)
             let f: Float32 = 440.0 * pow(2.0, (floor(c * 88.0) - 49.0) / 12.0)
             
             return f
@@ -155,18 +155,6 @@ func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float3
         
         /** --------------------------------  **/
         
-        func a(_ value1: UInt32) -> (UInt32) -> (UInt32) -> [Float32] {
-            func b(_ value2: UInt32) -> (UInt32) -> [Float32] {
-                func c(_ value3: UInt32) -> [Float32] {
-                    // You can use value1, value2, and value3 here to determine the result
-                    return [Float32(value1), Float32(value2), Float32(value3)]
-                }
-                return c
-            }
-            return b
-        }
-
-        
         func generateFrequencies(frame_count: Int32) -> [Float32] {
             let frame_indicies = incrementer(frame_count)
             var combined_frequency_samples: [Float32] = [Float32]() // [Float32](repeating: Float32.zero, count: frame_indicies.1.count)
@@ -176,11 +164,12 @@ func scale(min_new: Float32, max_new: Float32, val_old: Float32, min_old: Float3
                     harmonic = root * (3.0 / 2.0)
                     amplitude = 3.0
                 }
-                let amplitude_ : Float32 = scale(min_new: Float32.zero, max_new: amplitude, val_old: cos((tau * frame_indicies.1[kv.offset] * amplitude) - phase_offset), min_old: Float32.zero, max_old: cos(tau * pow(amplitude, 0.333)))
+                let envelope_: Float32 = scale(min_new: Float32.zero, max_new: amplitude, val_old: cos((tau * frame_indicies.1[kv.offset] * 2.0)), min_old: Float32.zero, max_old: cos(tau * pow(amplitude, 0.333)))
+                let amplitude_ : Float32 = scale(min_new: Float32.zero, max_new: amplitude, val_old: pow(cos(((Float32.pi * frame_indicies.1[kv.offset]))), 2.0), min_old: Float32.zero, max_old: cos(tau * pow(amplitude, 0.333)))
                 let root_      : Float32 = scale(min_new: Float32.zero, max_new: 1.0, val_old: cos(tau * frame_indicies.1[kv.offset] * root), min_old: Float32.zero, max_old: cos(tau * root))
                 let harmonic_  : Float32 = scale(min_new: Float32.zero, max_new: 1.0, val_old: cos(tau * frame_indicies.1[kv.offset] * harmonic), min_old: Float32.zero, max_old: cos(tau * harmonic))
 
-                return (amplitude_ * (root_ + harmonic_))
+                return (-amplitude_ * (root_ + harmonic_))
             }))
 //            (frame_indicies.0).enumerated() { (i, element) in
 //                if frame_indicies.0[Int(i)] == Int.zero {
