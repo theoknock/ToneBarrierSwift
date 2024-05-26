ae2f9a1a52c303c4ede9b7502cc4a0022330d898
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-26 16:38:07 -0400

    Functional play button in SwiftUI view

--- ContentView.swift
+++ ContentView.swift
@@ -6,41 +6,65 @@
 //
 
 import SwiftUI
+import AVFoundation
+import AVFAudio
 
 struct ContentView: View {
+    
+    let toneBarrierSapphire: Color = Color.init(hue: 206 / 360, saturation: 1, brightness: 1)
+    @State private var isPlaying: Bool = false
+    
+    var audioSession: AVAudioSession = AVAudioSession.sharedInstance()
+    var audioSignal: AVAudioSignal = AVAudioSignal()
+    
+    func audio() {
+        do {
+            if !(audioSignal.audio_engine.isRunning) {
+                try audioSignal.audio_engine.start()
+            } else {
+                audioSignal.audio_engine.pause()
+            }
+        } catch let error as NSError {
+            debugPrint("\(error.localizedDescription)")
+        }
+        isPlaying = audioSignal.audio_engine.isRunning
+    }
+    
+    
     var body: some View {
-        let divideValue = 2.0
         GeometryReader { proxy in
             ZStack(alignment: Alignment(horizontal: .center, vertical: .center), content: {
                 Color.black
-                HStack(spacing: 0) {
-                    Image(systemName: "waveform.path")
+                Image(systemName: "waveform.path")
+                    .resizable()
+                    .scaledToFit()
+                    .aspectRatio(1.0, contentMode: .fit)
+                    .frame(width: proxy.size.width)
+                    .clipShape(Rectangle())
+                    .foregroundStyle(toneBarrierSapphire.opacity(0.15))
+                Button(action: {
+                    audio()
+                }) {
+                    Image(systemName: isPlaying ? "stop" : "play")
                         .resizable()
                         .scaledToFit()
-                        .aspectRatio(1.0, contentMode: .fit)
-                        .frame(width: proxy.size.width)
+                        .aspectRatio(0.35, contentMode: .fit)
+                        .frame(width: proxy.size.width * 0.35)
                         .clipShape(Rectangle())
-                        .foregroundStyle(Color.init(uiColor: UIColor.systemBlue).opacity(0.15))
+                        .fontWeight(Font.Weight?.some(Font.Weight.thin))
+                        .foregroundStyle(toneBarrierSapphire)
+                        .shadow(color: toneBarrierSapphire.opacity(0.15), radius: 10)
                 }
-                HStack(alignment: .center, content: {
-                    Image(systemName: "play")
-                        .resizable()
-                        .scaledToFit()
-                        .aspectRatio(0.5, contentMode: .fit)
-                        .frame(width: proxy.size.width * 0.5)
-                        .clipShape(Rectangle())
-                        
-                        
-                        
-                        
-                        .foregroundStyle(Color.init(uiColor: UIColor.systemBlue))
-                })
+                .onAppear {
+                    do {
+                        try audioSession.setCategory(.playback, mode: .default, policy: .longFormAudio)
+                        try audioSession.setActive(true)
+                    } catch {
+                        print("Failed to set audio session category.")
+                    }
+                }
             })
-            .background {
-                
-            }
             .ignoresSafeArea()
-            
         }
     }
 }
